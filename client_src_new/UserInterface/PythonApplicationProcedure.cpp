d60c068825d0943ec51b5bd65561121d18a50d02
Amun <amun.3808@gmail.com>
2025-10-16 20:19:27 +0300

    Blocked WM_SYSCOMMAND with SC_KEYMENU and updated some things

--- client_src_new/UserInterface/PythonApplicationProcedure.cpp
+++ client_src_new/UserInterface/PythonApplicationProcedure.cpp
@@ -1,4 +1,50 @@
 // Find
+	case WM_ACTIVATEAPP:
+	{
+		// ...
+		if (m_isActivateWnd)
+		{
+			// ...
+		}
+		else
+		{
+			// ...
+
+// Add here
+			if (IsUserMovingMainWindow())
+				SetUserMovingMainWindow(false);
+		}
+	}
+
+
+
+// Find and change
+	case WM_KEYDOWN:
+	{
+		if (wParam == VK_ESCAPE && IsUserMovingMainWindow())
+			SetUserMovingMainWindow(false);
+
+		// ....
+	}
+	break;
+
+
+
+// Find and change
+	case WM_LBUTTONDOWN:
+		// ...
+
+		// Add
+		if (IsUserMovingMainWindow())
+			SetUserMovingMainWindow(false);
+
+		return 0;
+
+
+
+
+
+// Find
 case WM_EXITSIZEMOVE:
 {
 ...
@@ -40,3 +86,7 @@ case WM_EXITSIZEMOVE:
 	case WM_NCRBUTTONUP:
 	case WM_CONTEXTMENU:
 		return 0;
+	case WM_SYSCOMMAND:
+		if (wParam == SC_KEYMENU)
+			return 0;
+		break;
